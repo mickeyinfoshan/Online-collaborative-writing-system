ac25fa54c8335c6e979d6140a46b9318b6dd1aaf
Mickey <mickey@yeeuu.com>
2016-01-17 21:59:14 +0800

    comment api

---
+++ src/com/pad/api/CommentApi.java
@@ -0,0 +1,107 @@
+package com.pad.api;
+
+import java.util.List;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.pad.entity.Comment;
+
+@Component
+@Path("/comment")
+public class CommentApi {
+	@Autowired
+	protected SessionFactory sessionFactory;
+
+	protected Session getSession() {
+		return this.sessionFactory.openSession();
+	}
+
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+	
+	@GET
+	@Path("/{pad_id}/list")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Comment[] getCommentList(
+			@PathParam("pad_id") String pad_id
+	){
+		Session session = getSession();
+		String query = "from Comment C where C.pad_id=" + pad_id;
+		List<Comment> list = (List<Comment>)(session.createQuery(query).list());
+		session.close();
+		int size = list.size();
+		Comment[] comments = new Comment[size];
+		return (Comment[])(list.toArray(comments));
+	}
+	
+	@POST
+	@Path("/{pad_id}/create")
+	public String createComment(
+			@PathParam("pad_id") String pad_id,
+			@FormParam(value="created_time") String created_time,
+			@FormParam(value="content") String content,
+			@FormParam(value="user_name") String user_name,
+			@FormParam(value="user_id") String user_id
+		){
+		
+		Comment c = new Comment();
+		c.setContent(content);
+		c.setCreated_time(created_time);
+		c.setPad_id(pad_id);
+		c.setUser_id(user_id);
+		c.setUser_name(user_name);
+		Session session = getSession();
+		Transaction t = session.beginTransaction();
+		session.save(c);
+		t.commit();
+		session.close();
+		return "200";
+	}
+	
+	@POST
+	@Path("/{pad_id}/delete/{comment_id}")
+	public String deleteComment(
+				@PathParam("pad_id") String pad_id,
+				@PathParam("comment_id") int comment_id
+			){
+		Session session = getSession();
+		Transaction t = session.beginTransaction();
+		Comment c = (Comment)session.get(Comment.class, comment_id);
+		session.delete(c);
+		t.commit();
+		session.close();
+		return "200";
+	}
+	
+	@POST
+	@Path("/{pad_id}/update/{comment_id}")
+	public String updateComment(
+				@PathParam("pad_id") String pad_id,
+				@PathParam("comment_id") int comment_id
+			){
+		Session session = getSession();
+		Transaction t = session.beginTransaction();
+		Comment c = (Comment)session.get(Comment.class, comment_id);
+		session.save(c);
+		t.commit();
+		session.close();
+		return "200";
+	}
+}
--- src/com/pad/entity/Comment.java
+++ src/com/pad/entity/Comment.java
@@ -9,7 +9,11 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class Comment {
 	private int id;
-	private String name;
+	private String created_time;
+	private String user_name;
+	private String user_id;
+	private String content;
+	private String pad_id;
 	
 	@Id
 	@GeneratedValue
@@ -21,13 +25,46 @@ public class Comment {
 		this.id = id;
 	}
 
-	public String getName() {
-		return name;
+	public String getCreated_time() {
+		return created_time;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setCreated_time(String created_time) {
+		this.created_time = created_time;
 	}
+
+	public String getUser_name() {
+		return user_name;
+	}
+
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+
+	public String getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getPad_id() {
+		return pad_id;
+	}
+
+	public void setPad_id(String pad_id) {
+		this.pad_id = pad_id;
+	}
+	
 	
 
 }
