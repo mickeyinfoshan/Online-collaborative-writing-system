3d37a492f03948d1c145b00fbe80404fb40d5228
Mickey <mickey@yeeuu.com>
2016-01-17 17:18:27 +0800

    inject sessionFactory

--- src/com/pad/api/MyApi.java
+++ src/com/pad/api/MyApi.java
@@ -5,14 +5,21 @@ import javax.ws.rs.Path;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import com.pad.entity.Comment;
+
+@Component
 @Path("/my")
 public class MyApi {
-
+	
+	@Autowired
 	protected SessionFactory sessionFactory;
 
 	protected Session getSession() {
-		return this.sessionFactory.getCurrentSession();
+		return this.sessionFactory.openSession();
 	}
 
 	public SessionFactory getSessionFactory() {
@@ -26,6 +33,13 @@ public class MyApi {
 	@GET
 	@Path("/hehe")
 	public String hehe() {
+		Session s = getSession();
+		Transaction t = s.beginTransaction();
+		Comment c = new Comment();
+		c.setName("hehe");
+		s.save(c);
+		t.commit();
+		s.close();
 		return "200";
 	}
 }
