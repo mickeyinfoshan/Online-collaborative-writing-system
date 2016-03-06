a2f7965889619f3a7969cd857f4f459998b4de0b
Mickey <mickey@yeeuu.com>
2016-03-06 08:15:19 +0800

    修改线上接口路径

--- src/com/pad/util/PadServerApi.java
+++ src/com/pad/util/PadServerApi.java
@@ -1,7 +1,7 @@
 package com.pad.util;
 
 public class PadServerApi {
-	private static String padHost = "localhost";
+	private static String padHost = "121.40.97.89";
 	private static String padPort = "8081";
 	private static String padApiKey = "8f2f95ba0babc4a151d970b8acfbc00869cf3dce5b5ca6893343303d86049cd8";
 	public static String getBaseRequestUrl(String method) {
